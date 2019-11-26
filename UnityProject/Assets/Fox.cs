37348169fd2d93cf77f0ffe5e5b2730e513e8c01
Reduichi <q2339198@gmail.com>
2019-11-26 12:48:45 +0800

    作業3-1(未完成)

---
+++ UnityProject/Assets/Fox.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class Fox : MonoBehaviour
+{
+    [Header("移動速度"), Range(0f, 50f)]
+    public float speed;
+    public Rigidbody2D Rig;
+    public SpriteRenderer Sr ;
+
+    /// <summary>
+    /// 讓狐狸移動，並且增加剛體
+    /// </summary>
+    /// <param name="x"></param>
+    private void OnAnimatorMove(float x)
+    {
+        x = Input.GetAxisRaw("Horizontal") * speed;
+        transform.Translate(x * Time.deltaTime, 0, 0);
+        //Time.deltaTime 減慢時間
+
+        Rig.AddForce(new Vector2(x, 0)); // 增加剛體
+    }
+
+    /// <summary>
+    /// 判斷是否要轉向
+    /// </summary>
+    private void Turn() 
+    {
+        if (Input.GetKeyDown("a") )
+        {
+            Sr.flipX = true;
+        }
+    }
+
+    private void Start()
+    { 
+        Rig = gameObject.AddComponent<Rigidbody2D>();
+        Sr = gameObject.AddComponent<SpriteRenderer>();
+    }
+
+    private void Update() 
+    {
+        OnAnimatorMove(20);
+        Turn();
+    }
+}
